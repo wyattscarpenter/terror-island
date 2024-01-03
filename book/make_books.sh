64ea4ab8d36e6e7f156a4a6b3beceb0ade1f9640
wyattscarpenter <wyattscarpenter@gmail.com>
2024-01-03 07:36:10 -0800

    slouching towards a more-perfect, 2024, edition

--- book/make_books.sh
+++ book/make_books.sh
@@ -1,54 +1,62 @@
-ebook-convert --version || sudo apt install -y calibre #here I try to install ebook-convert if you don't have it. I only really try for ubuntu, however.
+#!/bin/bash
+set -euo pipefail #bash strict mode (we deliberately keep the default IFS, because we use it later to pass some command line arguments around.)
 
-if [ "$1" = "-inplace" -o "$1" = "--inplace" ] && [ -e site/ ] ; then #sometimes disable, for development testing purposes
+if [ "$#" -ge 2 ] || [ "$#" -eq 1 ] && ! [ "$#" -a "$1" = "-inplace" -o "$1" = "--inplace" ] ; then
+  echo "Usage: $0 [[-]-inplace]. Provided input: $0 $@"
+  exit 22
+fi
+
+if [ "$#" -ge 1 ] && [ "$#" -a "$1" = "-inplace" -o "$1" = "--inplace" ] && [ -e site/ ] ; then #sometimes disable clean runs, for development testing purposes
   echo "I see --inplace has been passed, and the dir exists, so I won't copy the new dir in again."
 else
   rm -r -f site/ #make sure we have a clean slate
-  cp -r ../mirror/www.terrorisland.net/ site/ # This step might take about a minute cold (on a HDD; on an SSD it should be shorter), but it's important to make sure we don't mess up the mirror by accident.
+  cp -r ../mirror/ site/ # This step might take about a minute cold (on a HDD; on an SSD it should be shorter), but it's important to make sure we don't mess up the mirror by accident.
 fi
+main_site_dir='site/www.terrorisland.net'
+
+ebook-convert --version || sudo apt install -y calibre #here I try to install ebook-convert if you don't have it. I only really try for ubuntu, however.
 
 echo 'Making Foreword into an "open ebook" with ebook-convert and then stealing the file...'
   ebook-convert foreword.md tmp-foreword --title "Foreword" >/dev/null
-  cp tmp-foreword/index.html site/2-foreword.md.html
+  cp tmp-foreword/index.html $main_site_dir/2-foreword.md.html
 rm -r -f tmp-foreword/
 
 echo 'Making Buzzsaw Review into an "open ebook" with ebook-convert and then stealing the files...'
-  ebook-convert site/etcetera/buzzsaw_review.pdf tmp-review --title "Buzzsaw Review" >/dev/null #make it into an "open ebook" with ebook-convert and then steal the files
-  cp tmp-review/index.html site/3-buzzsaw_review.pdf.html
-  cp tmp-review/index-1_1.jpg site/
+  ebook-convert $main_site_dir/etcetera/buzzsaw_review.pdf tmp-review --title "Buzzsaw Review" >/dev/null #make it into an "open ebook" with ebook-convert and then steal the files
+  cp tmp-review/index.html $main_site_dir/3-buzzsaw_review.pdf.html
+  cp tmp-review/index-1_1.jpg $main_site_dir/
 rm -r -f tmp-review/
 
-cp title_page.html site/1-title_page.html
-cp cover_back.html site/Z-cover_back.html
-cp cover_back.pdn.png site/
-mv site/strips/index.html site/strips/000-index.html # This is the strip index, the incomplete "Year One Archives"; not to be confused with the website index, which is just the home page.
-rm -f site/index.html # This deliberately deletes the old website index.html, as it's just the final strip, anyway. (And therefore the resulting book from it is jus a useless first page, frontmatter, and random strips you can reach from this first starting point.
-#tree site/strips --noreport -H . -T "Terror Island" >site/index.html #tree is a worse way to do it...
-cd site/
+cp title_page.html $main_site_dir/1-title_page.html
+cp cover_back.html $main_site_dir/Z-cover_back.html
+cp cover_back.pdn.png $main_site_dir/
+mv $main_site_dir/strips/index.html $main_site_dir/strips/000-index.html # This is the strip index, the incomplete "Year One Archives"; not to be confused with the website index, which is just the home page.
+rm -f $main_site_dir/index.html # This deliberately deletes the old website index.html, as it's just the final strip, anyway. (And therefore the resulting book from it is jus a useless first page, frontmatter, and random strips you can reach from this first starting point.
+
+cd $main_site_dir/
   find -name '*.html' -exec sh -c 'echo "<a href=\"{}\">`basename -s .html {}`</a>" ' \; >0-index.html
-cd ..
+cd -
+
 typeset_book () {
   # $1: file extension for desired file type, including the leading . (empty by default, which makes an OEB, mostly useful for debug)
   # $2: additional ebook-convert options to pass in at the end to facilitate your chosen format.
-  #ebook-convert site/strips/063.html books/terror_island_unofficial-wyattscarpenter-2023"$1"  --max-levels 0 #TODO: there is a blank page after #63 that defies reason. Also after the end page, but maybe that's just to get up to an even number of pages? Or maybe it just thinks the back cover is too big.
-  echo "Making ebook with following extension: $1"
-  ebook-convert site/0-index.html books/terror_island_unofficial-wyattscarpenter-2023"$1" \
-    --no-chapters-in-toc --max-toc-links 0 --breadth-first \
+  echo "Making ebook with following extension: ${1:-}"
+  ebook-convert $main_site_dir/0-index.html books/terror_island_unofficial-wyattscarpenter-2024"${1:-}" \
+    --no-chapters-in-toc --max-toc-links 0 --breadth-first --max-levels 1 \
     --search-replace replacements.txt \
     --language en --cover cover.pdn.png \
-    --authors "Ben Heaton&Lewis Powell" --book-producer "Wyatt S Carpenter" --pubdate 2023 --title "Terror Island" --rating 5 \
+    --authors "Ben Heaton&Lewis Powell" --book-producer "Wyatt S Carpenter" --pubdate 2024 --title "Terror Island" --rating 5 \
     --base-font-size 10 --smarten-punctuation --extra-css "* {box-sizing: border-box; font-size: normal;} p { text-indent: 1.5em; margin: 0em !important; padding-left: 0em !important; } h4 {margin-bottom: 0em; margin-top: 0.3em;} body {margin-left: 0em; margin-right: 0em;} div.title /* this is just styling from the original page that we wiped out by removing the CSS, and want to get back. */ { background:#eeeeee; color:#000000; padding:2px; border-left:15px solid #333333; border-bottom:1px solid #333333; font-size:small; font-weight:bold; } /*here we unsuccessfully try to correct nonsense it otherwise does*/ .calibre1 {margin: 0em !important} .strip1 {padding: 0em !important}" \
-    $2 #&>/dev/null
-
+    ${2:-} #&>/dev/null #you can use this to quiet the diagnostic messages if you like.
 }
 
-typeset_book ".pdf" '--custom-size 5.245x8.5  --pdf-page-margin-bottom 0 --pdf-page-margin-left 0 --pdf-page-margin-right 0 --pdf-page-margin-top 0 --margin-bottom 0 --margin-top 0 --margin-left 0 --margin-right 0 ' & # pdf options #interestingly, --pdf-footer-template <center>_PAGENUM_</center> gets an out-of-range error (calibre 6.11) #--paper-size a5 is closest to US trade paperback (6x9in) of the default options, but none are close enough, nor close enough to what we want...
-rm -r books/terror_island_unofficial-wyattscarpenter-2023
+typeset_book ".pdf" '--custom-size 5.245x8.5 --pdf-page-margin-bottom 0 --pdf-page-margin-left 0 --pdf-page-margin-right 0 --pdf-page-margin-top 0 --margin-bottom 0 --margin-top 0 --margin-left 0 --margin-right 0' & # pdf options #interestingly, --pdf-footer-template <center>_PAGENUM_</center> gets an out-of-range error (calibre 6.11) #--paper-size a5 is closest to US trade paperback (6x9in) of the default options, but none are close enough, nor close enough to what we want...
+rm -r -f books/terror_island_unofficial-wyattscarpenter-2024  #make sure we have a clean slate for this
 typeset_book & # This will create the OEB format
 wait
 
 #disable only for development purposes, sometimes
-if [ "$1" = "-inplace" -o "$1" = "--inplace" ] ; then
+if [ "${1:-}" = "-inplace" -o "${1:-}" = "--inplace" ] ; then
   echo "I see --inplace has been passed, so I won't remove the content dir to clean up."
 else
   rm -r site/
--- mirror/mirror.sh
+++ mirror/mirror.sh
@@ -1,2 +1,41 @@
-wget -erobots=off --mirror --convert-links --adjust-extension --page-requisites --convert-links http://www.terrorisland.net
-# Note that you canNOT just use terrorisland.net, you have to include the www. Or else maybe there's a setting in wget to deal with this, but I don't know it. Also, you have to ignore the robots.txt, because the webserve doesn't have one.
+#For testing purposes, you may pass this script an argument for which step to start at. 1 to run every step, 2 to skip the first step, 3 to skip the first two steps. 4 or above to exist immediately (there are only 3 steps).
+
+# Step 1: This line does a basic pass to get all of the webpages we need. If need be, you can also just run this line and get a pretty good result (there is no way to do that using this script; I'm just saying you can run it on your own):
+if [ "${1:-0}" -le 1 ] ; then
+  wget -erobots=off --mirror --convert-links --adjust-extension --page-requisites --span-hosts --domains=terrorisland.net,www.terrorisland.net http://www.terrorisland.net http://terrorisland.net # Note that you canNOT just use terrorisland.net, you have to include the www, because all the links in the html point to the www version. Also, you have to ignore the robots.txt, because the webserver doesn't have one.
+fi
+
+# Step 2: Getting the prereq from the "other domain" terrorisland.net was very easy, but for the rest of the internet we need to do it sort of "manually". We do this by downloading all the files again. This is bad, because wget should just have been able to do this all in one sweep. But it's only O(2n) bad, so, you know...
+if [ "${1:-0}" -le 2 ] ; then
+  wget -p --convert-links --span-hosts `find www.terrorisland.net/` #the link-rewriting is wrong unless we use wget on everything at once, therefore we do it this way/ That is, if we did it a different way, then 
+fi
+
+exit
+
+findreplace () {
+  # $1: string to grep for. Minimally-quoted, just for shell.
+  # $2: string to sed interpret. Maximally-quoted, as it must be interpreted as a regex (and also as a shell variable twice? Does that happen? If so, can I get around it with ${x@Q}?)
+  # $3: string to sed replace in. Medium-quoted, as there are fewer special meanings in this half of a regex replacement statement.
+  # See below for example usages of these
+  
+  #The */* means it explicitly does not match stuff in the root directory, which is good because otherwise it would replace the text in this very script. It hits every other directory, though.
+  grep -rliI $1 */* | xargs -i@ sed -i 's/'$2'/'$3'/g' @
+  # The grep is, like, recursive, only report file names of matches, case insensitive, and ignore binary files, in that order, iirc.
+}
+
+#Step 3: Handle the files we need to grab especially because the websites are down:
+if [ "${1:-0}" -le 3 ] ; then
+  findreplace 'http://waxintellectual.com/images/buttons/affiliation/factions/' 'http\:\/\/waxintellectual\.com\/images\/buttons\/affiliation\/factions\/' '..\/..\/waxintellectual\.com\/images\/buttons\/affiliation\/factions\/'
+  findreplace 'http://www.merehappenings.com/images/MH_title.gif' 'http\:\/\/www\.merehappenings.com\/images\/MH_title.gif' '..\/..\/merehappenings\.com\/images\/MH_title.gif'
+  findreplace 'http://www.webcomicbattle.com/images/comicbutton.gif' 'http\:\/\/www.webcomicbattle.com\/images\/comicbutton\.gif' '..\/..\/www.webcomicbattle.com\/images\/comicbutton\.gif'
+  findreplace 'http://www.photowebcomics.com/button.php?u=Factitious' 'http\:\/\/www\.photowebcomics\.com\/button\.php\?u\=Factitious' '..\/..\www.photowebcomics.com\/images\/vote.jpg'
+fi
+#the special directories are:
+# waxintellectual.com/
+#   (eg https://web.archive.org/web/20170926140438/http://waxintellectual.com/images/buttons/affiliation/factions/a-siddite-1.jpg )
+# www.merehappenings.com/
+#   https://web.archive.org/web/20110129082018/http://www.merehappenings.com/images/MH_title.gif
+# www.webcomicbattle.com/
+#   https://web.archive.org/web/20060718033636/http://www.webcomicbattle.com/images/comicbutton.gif
+# www.photowebcomics.com/ 
+#   www.photowebcomics.com/button.php?u=Factitious, which probably linked to images/vote.jpg at the time it was posted (so I have rewritten it to point it to there), and later linked to 4.jpg. I assume (I know it's been both at times in the past, but it's not really clear when is when).
